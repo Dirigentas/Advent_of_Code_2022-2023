0c529ff09a69de4c2a9607e1fa0079e8729995ca
Aras <minelga.aras@gmail.com>
2023-12-02 17:08:44 +0200

    1.2 near end

--- 2023/1.php
+++ 2023/1.php
@@ -1,17 +1,39 @@
 <?php
-
-$input = '14gxqgqsqqbxfpxnbccjc33eight
-eight2sevenkl
-mrjstg5onetwoeightgcczx8vgrgl
-9246
-ninetwo2crrqk2grsctqxqbcrmrdsqbrz9eight
-nlnineeightmndkqz8nineonenrqm
-nrhdxfsqvxcbcghf35eightthreeseven5';
+$input = 'two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen';
 
 $rows_array = explode("\r\n", $input);
 
+// 1.2
+$valid_digits = ["kalafijorasmanovardas", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+// print_r($valid_digits);
+
+foreach ($rows_array as $key => &$row) {
+    foreach ($valid_digits as $key => $digit) {
+        // echo $digit . " ";
+        echo strpos($row, $digit);
+        if (strpos($row, $digit) !== false) {
+            $row[strpos($row, $digit)] = $key;
+            // echo "LABASSSS";
+        }
+        // echo ".";
+    }
+    // echo "\n";
+}
+// remove reference
+unset($row);
+
+print_r($rows_array);
+
+
 $final_answer = 0;
 
+// 1.1
 foreach ($rows_array as $key => $row) {
     
     $row_answer = '';
@@ -21,32 +43,35 @@ foreach ($rows_array as $key => $row) {
     $reversed_row = '';
 
     // first number
-    foreach ($one_row_array as $key => $symbol) {
+    foreach ($one_row_array as $key1 => $symbol) {
         if (ctype_digit($symbol)) {
                 $row_answer .= $symbol;
+                // echo $key . "-" . $row . "-" . $row_answer;
                 break;
         }
     }
 
     // last number
-    foreach ($one_row_array as $key => $symbol) {
+    foreach ($one_row_array as $key2 => $symbol) {
         $reversed_row = $symbol . $reversed_row;
     }
 
     $reversed_row = str_split($reversed_row);
 
-    foreach ($reversed_row as $key => $symbol) {
+    foreach ($reversed_row as $key3 => $symbol) {
         if (ctype_digit($symbol)) {
             $row_answer .= $symbol;
             echo $row_answer;
-            echo "--------";
+            // echo ".";
             break;
         }
     }
+    echo "\r\n";
     $final_answer += $row_answer;
     $reversed_row = '';
 }
-    // echo "________";
-    // echo $final_answer;
-
+    echo "________";
+    echo "\r\n";
+    echo $final_answer;
+//54031, 54087 netiko, per mazas
 ?>
